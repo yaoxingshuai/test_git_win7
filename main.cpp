75ef99c6ac176ffa4cc08289a5169595e8b7dd4d
sky <yaoxing@bjtu.edu.cn>
2017-10-08 12:31:57 +0800

    change something

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ int main() {
     vec.push_back(2);
     vec.push_back(3);
     vec.push_back(4);
-    vec.push_back(6);
     vec.push_back(5);
+    vec.push_back(6);
     vec.push_back(7);
 
     for (auto i: vec)
